d1c79282336997451cf6c710f0ef11f02cf56c1d
Destin Moulton <destin.moulton@gmail.com>
2020-08-27 18:38:47 -0600

    Added code for the messenger setup.

--- source/background.ts
+++ source/background.ts
@@ -1,1 +1,11 @@
-import "./options-storage";
+import optionsStorage from "./options-storage";
+//import { browser } from "webextension-polyfill-ts";
+import messenger from "./messenger";
+
+messenger.setup();
+console.log("tiddlybench :: background.ts LOADED V2");
+
+(async function() {
+    const options = await optionsStorage.getAll();
+    console.log(options);
+})();
