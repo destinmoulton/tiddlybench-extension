0d4d5f13aad1d3cfdd0fe288ce0224bf6050b5a8
Destin Moulton <destin.moulton@gmail.com>
2020-08-27 18:38:30 -0600

    Added test fetch code.

--- source/api.ts
+++ source/api.ts
@@ -1,6 +1,22 @@
+import messenger from "./messenger";
+
+import optionsStorage from "./options-storage";
+import base64 from "base-64";
+
 class API {
-    test() {
-        console.log("testing");
+    async test() {
+        var options = await optionsStorage.getAll();
+        messenger.log(options);
+        const headers = new Headers({
+            Authorization: `Basic ${base64.encode(
+                `${options.username}:${options.password}`
+            )}`,
+        });
+        return fetch(options.url, { headers }).then((response) => {
+            if (!response.ok) throw new Error(response.status.toString());
+            messenger.log("test :: ", response);
+            return response.json();
+        });
     }
 }
 
