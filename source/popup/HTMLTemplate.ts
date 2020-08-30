e919d0101f45c4d30e4382a216819e4e7838ebd9
Destin Moulton <destin.moulton@gmail.com>
2020-08-30 16:36:02 -0600

    Added loading animation via css

--- source/popup/HTMLTemplate.ts
+++ source/popup/HTMLTemplate.ts
@@ -1,37 +1,64 @@
 import logger from "../lib/logger";
 class HTMLTemplate {
+    _$root: HTMLElement;
+    _loadingAnimationOldHTML: string = "";
+
+    constructor() {
+        this._$root = this._getRootElement();
+    }
+
+    _getRootElement(): HTMLElement {
+        const $root = document.getElementById("root");
+
+        if (!$root) {
+            throw new Error("Unable to find root element.");
+        }
+
+        return $root;
+    }
     _compile(templateID: string, data: any): string {
         const $tmpl = document.getElementById(templateID);
 
         logger.log("PopupTemplates :: render()", templateID);
 
-        if ($tmpl) {
-            var tmplTxt = $tmpl.innerText;
-
-            const keys = Object.keys(data);
-
-            keys.forEach((key) => {
-                // prettier-ignore
-                const rgx = new RegExp('{{\\s*' + key + '\\s*}}', "g");
-                logger.log(rgx);
-                if (tmplTxt.search(rgx) >= 0) {
-                    logger.log("PopupTemplate :: _compile :: search found");
-                    tmplTxt = tmplTxt.replace(rgx, data[key]);
-                }
-            });
-
-            return tmplTxt;
+        if (!$tmpl) {
+            throw new Error("Unable to find the template " + templateID);
         }
-        return "";
+        var tmplTxt = $tmpl.innerText;
+
+        const keys = Object.keys(data);
+
+        keys.forEach((key) => {
+            // prettier-ignore
+            const rgx = new RegExp('{{\\s*' + key + '\\s*}}', "g");
+            logger.log(rgx);
+            if (tmplTxt.search(rgx) >= 0) {
+                logger.log("PopupTemplate :: _compile :: search found");
+                tmplTxt = tmplTxt.replace(rgx, data[key]);
+            }
+        });
+
+        return tmplTxt;
     }
 
     _render(html: string) {
-        const $root = document.getElementById("root");
+        logger.log("PopupTemplate :: adding html");
+        this._$root.innerHTML = html;
+    }
 
-        if ($root) {
-            logger.log("PopupTemplate :: adding html");
-            $root.innerHTML = html;
-        }
+    _loadingAnimation(animationText: string) {
+        this._loadingAnimationOldHTML = this._$root.innerHTML;
+
+        const loaderHTML = this._compile("tmpl-loading-animation", {
+            text: animationText,
+        });
+
+        this._render(loaderHTML);
+    }
+
+    // Return the root to the old HTML
+    _cancelLoadingAnimation() {
+        this._$root.innerHTML = this._loadingAnimationOldHTML;
     }
 }
 
