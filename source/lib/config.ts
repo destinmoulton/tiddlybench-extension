fc4b3bc69559a0ba8c5c7e86a967546d970dbcff
Destin Moulton <destin.moulton@gmail.com>
2020-08-30 15:57:24 -0600

    Added wrapper methods to getAll and syncForm

--- source/lib/config.ts
+++ source/lib/config.ts
@@ -1,7 +1,7 @@
 import OptionsSync from "webext-options-sync";
 import { ErrValTuple } from "../types";
 class Config {
-    _sync = {};
+    _sync: any;
 
     constructor() {
         this._sync = new OptionsSync({
@@ -9,7 +9,7 @@ class Config {
                 url: "",
                 username: "",
                 password: "",
-                is_context_menu_enabled: true,
+                context_menu_visibility: "on",
             },
             migrations: [OptionsSync.migrations.removeUnused],
             logging: true,
@@ -17,7 +17,7 @@ class Config {
     }
 
     async get(key: string): Promise<ErrValTuple> {
-        const options = await this._sync.getAll();
+        const options = await this.getAll();
 
         if (options.hasOwnProperty(key)) {
             return [options[key], null];
@@ -27,6 +27,14 @@ class Config {
             null,
             new Error("That key does not exist in the synced options."),
         ];
+    }
+
+    async getAll(): Promise<any> {
+        return this._sync.getAll();
+    }
+
+    syncForm(formID: string) {
+        this._sync.syncForm(formID);
     }
 }
 
