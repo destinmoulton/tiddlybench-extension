d5730d66c4994f189e10b0c72cb57248138de742
Destin Moulton <destin.moulton@gmail.com>
2020-09-01 11:55:39 -0600

    Rebuilt options as partials.

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,23 @@ var gulp = require("gulp");
 var partial = require("gulp-html-partial");
 
 // compile the popup partials into one html file
-gulp.task("compile-partials", function() {
+gulp.task("compile-popup-partials", function() {
     return gulp
         .src("./templates/popup/popup.html")
         .pipe(partial({ basePath: "./templates/popup/partials/" }))
         .pipe(gulp.dest("./distribution/popup/"));
 });
 
+gulp.task("compile-options-partials", function() {
+    return gulp
+        .src("./templates/options/options.html")
+        .pipe(partial({ basePath: "./templates/options/partials/" }))
+        .pipe(gulp.dest("./distribution/options/"));
+});
 gulp.task("watch", function() {
     gulp.watch(
         "templates/**/*.html",
         { ignoreInitial: false },
-        gulp.series(["compile-partials"])
+        gulp.series(["compile-popup-partials", "compile-options-partials"])
     );
 });
