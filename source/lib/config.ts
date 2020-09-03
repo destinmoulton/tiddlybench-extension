d1534dfd30b3970bbecf6969459e3370fc265bd3
Destin Moulton <destin.moulton@gmail.com>
2020-09-03 10:39:47 -0600

    Removed abstract methods; refactored for AbstractStorage

--- source/lib/config.ts
+++ source/lib/config.ts
@@ -1,19 +1,19 @@
-import { browser } from "webextension-polyfill-ts";
 import _ from "lodash";
+import AbstractStorage, { StorageElement } from "./AbstractStorage";
 
 /** The option name corresponds to the input id */
-interface ISettings {
+interface ISettings extends StorageElement {
     url: string;
     username: string;
     password: string;
     context_menu_visibility: string;
-    [key: string]: ISettings[keyof ISettings];
 }
-class Config {
+class Config extends AbstractStorage<ISettings> {
     _settingDefaults: ISettings;
     _storageKey: string;
 
     constructor() {
+        super();
         this._settingDefaults = {
             url: "",
             username: "",
@@ -22,50 +22,6 @@ class Config {
         };
 
         this._storageKey = "settings";
-    }
-
-    async _setAll(settings: ISettings) {
-        return browser.storage.local.set({ [this._storageKey]: settings });
-    }
-
-    async getAll(): Promise<ISettings> {
-        const setObj: any = await (<Promise<any>>(
-            browser.storage.local.get(this._storageKey)
-        ));
-
-        if (setObj.hasOwnProperty(this._storageKey)) {
-            return <ISettings>setObj[this._storageKey];
-        }
-
-        try {
-            // There is nothing, so set the default value
-            await this._setAll(this._settingDefaults);
-        } catch (err) {
-            throw err;
-        }
-
-        return this._settingDefaults;
-    }
-
-    async set(key: string, value: any) {
-        const settings: ISettings = await this.getAll();
-        if (!settings.hasOwnProperty(key)) {
-            throw new Error(`${key} is not defined as a setting.`);
-        }
-
-        settings[key] = value;
-
-        return await this._setAll(settings);
-    }
-
-    async get(key: string): Promise<any> {
-        const settings = await this.getAll();
-
-        if (!settings.hasOwnProperty(key)) {
-            throw new Error(`${key} is not defined as a possible setting.`);
-        }
-
-        return settings[key];
     }
 
     async reset(formID: string) {
