6030ddb356e2a66c73c3eca4406162dcfb5b583e
Destin Moulton <destin.moulton@gmail.com>
2020-09-05 17:30:30 -0600

    Created DateFormatMap for tiddler title date parsing

---
+++ source/lib/DateFormatMap.ts
@@ -0,0 +1,40 @@
+/**
+ * Map of tiddlywiki date format to momentjs
+ * date format.
+ *
+ * https://tiddlywiki.com/static/DateFormat.html
+ * https://momentjs.com/docs/#/displaying/
+ */
+
+export default {
+    DDD: "dddd",
+    ddd: "ddd",
+    DD: "D",
+    "0DD": "DD",
+    WW: "W",
+    "0WW": "WW",
+    MMM: "MMMM",
+    mmm: "MMM",
+    MM: "M",
+    "0MM": "MM",
+    YYYY: "YYYY",
+    YY: "YY",
+    wYYYY: "",
+    wYY: "",
+    hh: "H",
+    "0hh": "HH",
+    hh12: "h",
+    "0hh12": "hh",
+    mm: "m",
+    "0mm": "mm",
+    ss: "s",
+    "0ss": "ss",
+    XXX: "",
+    "0XXX": "",
+    am: "a",
+    pm: "a",
+    AM: "A",
+    PM: "A",
+    TZD: "",
+    "[UTC]": "",
+};
