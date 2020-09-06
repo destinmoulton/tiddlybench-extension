d173afd76af7a47caf7f416c484eda9aba055edb
Destin Moulton <destin.moulton@gmail.com>
2020-09-05 18:02:26 -0600

    Added context menu entries.

--- source/background/contextmenu.ts
+++ source/background/contextmenu.ts
@@ -1,9 +1,12 @@
 import { browser } from "webextension-polyfill-ts";
+import _ from "lodash";
 import config from "../lib/storage/config";
+import editortabs from "../lib/editortabs";
 //import messenger from "../lib/messenger";
 //import logger from "../lib/logger";
 class ContextMenu {
     isContextMenuEnabled: string = "off";
+    doesContextMenuExist: boolean = false;
 
     async initialize() {
         await this.reconfigure();
@@ -23,26 +26,64 @@ class ContextMenu {
 
     _onContextMenuCreated() {
         // Setup the context menu event listeners
-        browser.contextMenus.onClicked.addListener((info, tab) => {
-            switch (info.menuItemId) {
-                case "tb-ctxt-add-to-tiddler":
-                    console.log(tab);
-                    browser.tabs.create({
-                        url: "tabs/tiddler-editor.html",
+        if (
+            !browser.contextMenus.onClicked.hasListener(
+                this._onContextMenuClicked.bind(this)
+            )
+        ) {
+        } else {
+            console.log("context menu is already created");
+        }
+    }
+
+    async _onContextMenuClicked(
+        info: browser.contextMenus.OnClickData,
+        tab: browser.tabs.Tab | undefined
+    ) {
+        switch (info.menuItemId) {
+            case "tb-ctxt-add-to-tiddler":
+                if (tab && info.pageUrl && tab.title && info.selectionText) {
+                    editortabs.create(
+                        info.pageUrl,
+                        tab.title,
+                        info.selectionText
+                    );
+                }
+                break;
+            case "tb-ctxt-add-to-inbox":
+                try {
+                    await browser.notifications.create({
+                        type: "basic",
+                        title: "TiddlyBench",
+                        message: "Added selection to Inbox.",
                     });
-                    break;
-            }
-        });
+                } catch (err) {
+                    throw err;
+                }
+                break;
+        }
     }
 
     enableContextMenu() {
         browser.contextMenus.create(
             {
                 id: "tb-ctxt-add-to-tiddler",
-                title: "Add selection to Tiddler.",
+                title: "Create new Tiddler from selection.",
                 contexts: ["selection"],
             },
             this._onContextMenuCreated.bind(this)
+        );
+        browser.contextMenus.create(
+            {
+                id: "tb-ctxt-add-to-inbox",
+                title: "Add selection to Inbox Tiddler.",
+                contexts: ["selection"],
+            },
+            this._onContextMenuCreated.bind(this)
+        );
+        this.doesContextMenuExist = true;
+        browser.contextMenus.onClicked.addListener(
+            this._onContextMenuClicked.bind(this)
         );
     }
 
