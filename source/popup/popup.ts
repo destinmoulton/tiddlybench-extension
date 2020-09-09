3d6871b9a136c9a210fe0f248f3d2a28f339f9bd
Destin Moulton <destin.moulton@gmail.com>
2020-09-08 19:04:25 -0600

    Added a popup class to handle checking the status

--- source/popup/popup.ts
+++ source/popup/popup.ts
@@ -1,7 +1,38 @@
+import HTMLTemplate from "./HTMLTemplate";
 import mainmenu from "./MainMenu";
 import quickAddTiddler from "./QuickAddTiddler";
+import api from "../lib/api";
+import extensionurls from "../lib/extensionurls";
 
 window.addEventListener("load", function() {
-    mainmenu.show();
-    quickAddTiddler.display();
+    const popup = new Popup();
+    popup.initialize();
 });
+
+class Popup extends HTMLTemplate {
+    async initialize() {
+        this._loadingAnimation("Checking server status...");
+        const status = await api.getStatus();
+
+        console.log(status);
+        if (!status.ok) {
+            const html = this._compile("tmpl-not-connected", {});
+
+            this._render(html);
+
+            const $button = document.getElementById(
+                "tb-popup-configure-button"
+            );
+
+            if ($button) {
+                $button.addEventListener(
+                    "click",
+                    extensionurls.openSettingsTab
+                );
+            }
+        } else {
+            mainmenu.show();
+            quickAddTiddler.display();
+        }
+    }
+}
