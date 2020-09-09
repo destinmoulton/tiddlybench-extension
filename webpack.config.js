4b0714f4859538f60e07bad145b23896db2c969e
Destin Moulton <destin.moulton@gmail.com>
2020-09-09 16:34:16 -0600

    Added browser external

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             },
         ],
     },
-    externals: { lodash: "_" },
+    externals: { lodash: "_", "webextension-polyfill-ts": "browser" },
     plugins: [
         new SizePlugin(),
         new CopyWebpackPlugin([
