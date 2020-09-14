ca7ba080873e1e86ec1c4d7be99873518939c962
Destin Moulton <destin.moulton@gmail.com>
2020-09-13 18:09:03 -0600

    Added code to add a tiddler

--- source/popup/QuickAddTiddler.ts
+++ source/popup/QuickAddTiddler.ts
@@ -1,10 +1,17 @@
 import HTMLTemplate from "./HTMLTemplate";
+//import formatit from "../lib/formatting/formatit";
+//import config from "../lib/storage/config";
+import dom from "../lib/dom";
+//import { browser } from "webextension-polyfill-ts";
+import messenger from "../lib/messenger";
 class QuickAddTiddler extends HTMLTemplate {
-    $container: HTMLElement | undefined;
+    $container: HTMLElement | null;
+    $textarea: HTMLInputElement | null;
 
     constructor() {
         super();
-        this.$container = undefined;
+        this.$container = null;
+        this.$textarea = null;
     }
 
     display() {
@@ -13,34 +20,60 @@ class QuickAddTiddler extends HTMLTemplate {
         console.log(html);
         this._append(html);
 
-        this.$container = <HTMLElement>(
-            document.getElementById("tb-popup-quickadd-box")
-        );
+        this.$container = <HTMLElement>dom("#tb-popup-quickadd-box");
 
         // Focus on the textarea
-        const $textarea = document.getElementById("tb-popup-quickadd-contents");
-        if ($textarea) {
-            $textarea.focus();
-        }
+        this.$textarea = <HTMLInputElement>dom("#tb-popup-quickadd-contents");
+        this.$textarea.focus();
 
         // Setup the handler
-        const $submit = document.getElementById(
-            "tb-popup-quickadd-submit-button"
-        );
-        if ($submit) {
-            $submit.addEventListener(
-                "click",
-                this.handleClickSubmit.bind(this)
-            );
+        const $submit = <HTMLElement>dom("#tb-popup-quickadd-submit-button");
+        $submit.addEventListener("click", this.handleClickSubmit.bind(this));
+    }
+
+    async handleClickSubmit() {
+        const animation = this._getLoadingAnimationHTML("Adding...");
+
+        if (this.$container) {
+            if (this.validate()) {
+                const text = this.$textarea ? this.$textarea.value : "";
+                this.$container.innerHTML = animation;
+                try {
+                    messenger.send(
+                        {
+                            dispatch: "tiddler",
+                            type: "journal",
+                            packet: { text },
+                        },
+                        this.handleSubmitResponse.bind(this)
+                    );
+                    // await browser.notifications.create({
+                    //     type: "basic",
+                    //     iconUrl: "../icon.png",
+                    //     message: "Validated",
+                    //     title: "Test title",
+                    // });
+                } catch (err) {
+                    console.error(err.message);
+                }
+            }
         }
     }
 
-    handleClickSubmit() {
-        const html = this._getLoadingAnimationHTML("Adding...");
+    /**
+     * Handle the messenger response
+     *
+     * @param message any
+     */
+    handleSubmitResponse(message: any) {
+        console.log("handleSubmitResponse", message);
+    }
 
-        if (this.$container) {
-            this.$container.innerHTML = html;
+    validate() {
+        if (this.$textarea && this.$textarea.value.trim() === "") {
+            return false;
         }
+        return true;
     }
 }
 
