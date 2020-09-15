c2987162543ed8d34018776b0d81d7961219c929
Destin Moulton <destin.moulton@gmail.com>
2020-09-15 17:57:03 -0600

    Refactored to use instance vars

--- source/popup/popup.ts
+++ source/popup/popup.ts
@@ -1,18 +1,30 @@
 import HTMLTemplate from "./HTMLTemplate";
 import mainmenu from "./MainMenu";
-import quickAddTiddler from "./QuickAddTiddler";
-import api from "../lib/api";
+import API from "../lib/API";
 import extensionurls from "../lib/extensionurls";
-
+import Messenger from "../lib/Messenger";
+import ConfigStorage from "../lib/storage/ConfigStorage";
+import QuickAddTiddler from "./QuickAddTiddler";
 window.addEventListener("load", function() {
-    const popup = new Popup();
+    const configStorage = new ConfigStorage();
+    const api = new API(configStorage);
+    const messenger = new Messenger(configStorage);
+    const quickAddTiddler = new QuickAddTiddler(messenger);
+    const popup = new Popup(api, quickAddTiddler);
     popup.initialize();
 });
 
 class Popup extends HTMLTemplate {
+    _api: API;
+    _quickAddTiddler: QuickAddTiddler;
+    constructor(api: API, quickAddTiddler: QuickAddTiddler) {
+        super();
+        this._api = api;
+        this._quickAddTiddler = quickAddTiddler;
+    }
     async initialize() {
         this._loadingAnimation("Checking server status...");
-        const status = await api.getStatus();
+        const status = await this._api.getStatus();
 
         console.log(status);
         if (!status.ok) {
@@ -32,7 +44,7 @@ class Popup extends HTMLTemplate {
             }
         } else {
             mainmenu.show();
-            quickAddTiddler.display();
+            this._quickAddTiddler.display();
         }
     }
 }
