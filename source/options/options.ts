fc56b7f4d61a5e4bed1f7833394f228066457ad5
Destin Moulton <destin.moulton@gmail.com>
2020-09-15 17:14:28 -0600

    Refactored out all code

--- source/options/options.ts
+++ source/options/options.ts
@@ -1,50 +1,14 @@
-import config from "../lib/storage/config";
-import api from "../lib/api";
-import logger from "../lib/logger";
+/**
+ * options.ts
+ *
+ * Initialize the OptionsForm
+ */
 
-import { API_Result } from "../types";
-//import { browser } from "webextension-polyfill-ts";
+import ConfigStorage from "../lib/storage/ConfigStorage";
+import OptionsForm from "./OptionsForm";
 
-window.addEventListener("load", startOptions);
-function startOptions() {
-    logger.log("StartOptions handler running");
-    config.syncForm("options-form");
-}
-const testConnection: HTMLElement = <HTMLElement>(
-    document.getElementById("tb-test-connection")
-);
-testConnection.addEventListener("click", testConnectionOptions);
+const configStorage = new ConfigStorage();
+const optionsForm = new OptionsForm(configStorage);
 
-const $success = <HTMLElement>(
-    document.getElementById("tb-connection-success-message")
-);
-
-const $error = <HTMLElement>(
-    document.getElementById("tb-connection-error-message")
-);
-
-const $reset = <HTMLElement>document.getElementById("reset-settings");
-
-$reset.addEventListener("click", shouldReset);
-
-async function testConnectionOptions(): Promise<API_Result> {
-    const res = await api.getStatus();
-    logger.log("options :: test()", res);
-    if (res.ok) {
-        $success.style.display = "inline-block";
-    } else {
-        $error.innerText = <string>res.message;
-        $error.style.display = "inline-block";
-    }
-    return res;
-}
-
-async function shouldReset() {
-    const isOk = confirm(
-        "Are you sure you want reset the settings back to default?"
-    );
-
-    if (isOk) {
-        await config.reset("options-form");
-    }
-}
+// Initialize the OptionsForm
+window.addEventListener("load", optionsForm.initialize.bind(optionsForm));
