abb3522235857197a4047b185f289f3060288b45
Destin Moulton <destin.moulton@gmail.com>
2020-09-15 17:33:04 -0600

    Renamed to Inbox

--- source/lib/tiddlers/Inbox.ts
+++ source/lib/tiddlers/Inbox.ts
@@ -1,15 +1,14 @@
 import AbstractTiddler from "./AbstractTiddler";
-import config from "../storage/config";
 import formatit from "../formatting/formatit";
 class Inbox extends AbstractTiddler {
     async _populateTitle() {
-        const title = await config.get("inbox_tiddler_title");
+        const title = await this._configStorage.get("inbox_tiddler_title");
         this._tiddlerTitle = formatit(title);
     }
 
     async addText(text: string) {
-        const text_prefix = await config.get("inbox_text_prefix");
-        const text_suffix = await config.get("inbox_text_suffix");
+        const text_prefix = await this._configStorage.get("inbox_text_prefix");
+        const text_suffix = await this._configStorage.get("inbox_text_suffix");
 
         let newText =
             formatit(text_prefix) + formatit(text) + formatit(text_suffix);
@@ -19,4 +18,4 @@ class Inbox extends AbstractTiddler {
         this.setTiddlerText(tiddlerText);
     }
 }
-export default new Inbox();
+export default Inbox;
