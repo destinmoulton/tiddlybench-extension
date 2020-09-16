a5a0c5f79b067add3a1556d8149cd1923d5969aa
Destin Moulton <destin.moulton@gmail.com>
2020-09-16 17:31:08 -0600

    Moved messenger handler to BackgroundActions

--- source/lib/Messenger.ts
+++ source/lib/Messenger.ts
@@ -2,8 +2,6 @@ import { browser } from "webextension-polyfill-ts";
 
 import API from "./API";
 import ConfigStorage from "./storage/ConfigStorage";
-import Journal from "./tiddlers/Journal";
-import Inbox from "./tiddlers/Inbox";
 
 class Messenger {
     _api: API;
@@ -13,59 +11,17 @@ class Messenger {
         this._api = api;
         this._configStorage = configStorage;
     }
-    setupListener() {
+    setupListener(
+        listener: (
+            data: any,
+            sender: browser.runtime.MessageSender
+        ) => Promise<any>
+    ) {
         /**
          * The addListener event handler MUST return a
          * Promise (resolve or reject)
          */
-        browser.runtime.onMessage.addListener(async (data, sender) => {
-            if (data.dispatch === "tiddler") {
-                switch (data.type) {
-                    case "journal": {
-                        const journal = new Journal(
-                            this._configStorage,
-                            this._api
-                        );
-                        await journal.initialize();
-                        await journal.addText(data.packet.text);
-                        const res = await journal.submit();
-                        if (res.ok) {
-                            return Promise.resolve({
-                                ok: true,
-                                message: "Journal text added.",
-                            });
-                        } else {
-                            return Promise.reject({
-                                ok: false,
-                                message: "Failed to add the Journal text.",
-                            });
-                        }
-                    }
-                    case "inbox": {
-                        const inbox = new Inbox(this._configStorage, this._api);
-                        await inbox.initialize();
-                        await inbox.addText(data.packet.text);
-                        const res = await inbox.submit();
-                        if (res.ok) {
-                            return Promise.resolve({
-                                ok: true,
-                                message: "Inbox text added.",
-                            });
-                        } else {
-                            return Promise.reject({
-                                ok: false,
-                                message: "Failed to add the Inbox text.",
-                            });
-                        }
-                    }
-                }
-            }
-            return Promise.reject({
-                ok: false,
-                sender,
-                message: "No dispatch or type parameters provided in the data.",
-            });
-        });
+        browser.runtime.onMessage.addListener(listener);
     }
 
     async send(
