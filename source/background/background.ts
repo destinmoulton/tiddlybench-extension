5484da83171b476d72e7352e066cae04ba3ab4ea
Destin Moulton <destin.moulton@gmail.com>
2020-09-15 18:04:43 -0600

    Instantiated vars for TiddlyBench

--- source/background/background.ts
+++ source/background/background.ts
@@ -1,6 +1,19 @@
+import API from "../lib/API";
+import ConfigStorage from "../lib/storage/ConfigStorage";
+import ContextMenu from "./ContextMenu";
+import Messenger from "../lib/Messenger";
 import TiddlyBench from "./TiddlyBench";
 
 (async function() {
-    const tiddlyBench = new TiddlyBench();
+    const configStorage = new ConfigStorage();
+    const api = new API(configStorage);
+    const contextMenu = new ContextMenu(configStorage);
+    const messenger = new Messenger(api, configStorage);
+    const tiddlyBench = new TiddlyBench(
+        api,
+        configStorage,
+        contextMenu,
+        messenger
+    );
     await tiddlyBench.initialize();
 })();
