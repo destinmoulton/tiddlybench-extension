81dd77d61cd0b1d87185231d5c74a65ccd42d940
Destin Moulton <destin.moulton@gmail.com>
2020-09-16 17:30:51 -0600

    Added handler for context menus

--- source/background/background.ts
+++ source/background/background.ts
@@ -1,4 +1,5 @@
 import API from "../lib/API";
+import BackgroundActions from "./BackgroundActions";
 import ConfigStorage from "../lib/storage/ConfigStorage";
 import ContextMenu from "./ContextMenu";
 import Messenger from "../lib/Messenger";
@@ -7,10 +8,20 @@ import TiddlyBench from "./TiddlyBench";
 (async function() {
     const configStorage = new ConfigStorage();
     const api = new API(configStorage);
-    const contextMenu = new ContextMenu(configStorage);
     const messenger = new Messenger(api, configStorage);
+    const backgroundActions = new BackgroundActions(
+        api,
+        configStorage,
+        messenger
+    );
+    const contextMenu = new ContextMenu(
+        configStorage,
+        messenger,
+        backgroundActions.handleContextMenuClicks.bind(backgroundActions)
+    );
     const tiddlyBench = new TiddlyBench(
         api,
+        backgroundActions,
         configStorage,
         contextMenu,
         messenger
