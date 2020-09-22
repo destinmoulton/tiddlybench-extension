fa525609702fd7766fb15582972e956fd7098e7b
Destin Moulton <destin.moulton@gmail.com>
2020-09-22 10:56:37 -0600

    Rebuilt tiddlerdrafts as ContextMenuStorage

---
+++ source/lib/storage/ContextMenusStorage.ts
@@ -0,0 +1,57 @@
+/**
+ * ContextMenusStorage.ts
+ *
+ * @class ContextMenusStorage
+ *
+ * Store information about additional
+ * context menus available for the user.
+ *
+ * The user can add destination tiddlers that
+ * are available via the TiddlyBench context menu.
+ */
+import _ from "lodash";
+import AbstractStorage, { StorageElement } from "./AbstractStorage";
+import ConfigStorage from "./ConfigStorage";
+import { ICustomDestination, ISelectionCache } from "../../types";
+//import { v4 } from "uuid";
+
+interface IContextMenuCache extends StorageElement {
+    destinations: ICustomDestination[];
+    selection_cache: ISelectionCache | null;
+}
+class ContextMenusStorage extends AbstractStorage<IContextMenuCache> {
+    _configStorage: ConfigStorage;
+    _storageDefaults: IContextMenuCache;
+    _storageKey: string;
+    _cacheKey: string;
+    _destinationsKey: string;
+
+    constructor(configStorage: ConfigStorage) {
+        super();
+        this._configStorage = configStorage;
+        this._cacheKey = "selection_cache";
+        this._destinationsKey = "destinations";
+        this._storageDefaults = {
+            destinations: [],
+            selection_cache: null,
+        };
+
+        this._storageKey = "context_menu_cache";
+    }
+
+    async addSelectionCache(pageUrl: string, pageTitle: string) {
+        const cache = {
+            page_url: pageUrl,
+            page_title: pageTitle,
+        };
+        await this.set(this._cacheKey, cache);
+    }
+
+    async getSelectionCache() {
+        return await this.get(this._cacheKey);
+    }
+
+    async addCustomDestination(tiddlerTitle: string) {}
+}
+
+export default ContextMenusStorage;
