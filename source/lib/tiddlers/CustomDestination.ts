286d8c019139b5bbfc1a06511bda7fdc3a620d97
Destin Moulton <destin.moulton@gmail.com>
2020-10-14 15:22:42 -0600

    Added options parameter.

--- source/lib/tiddlers/CustomDestination.ts
+++ source/lib/tiddlers/CustomDestination.ts
@@ -2,16 +2,16 @@ import { IDispatchOptions } from "../../types";
 import AbstractTiddler from "./AbstractTiddler";
 class CustomDestination extends AbstractTiddler {
     async populateTitle(options: IDispatchOptions) {
-        if (!options.id) {
+        if (!options.packet.cache_id) {
             throw new Error("id is not defined in the options");
         }
         const dest = await this._contextMenuStorage.getCustomDestinationByID(
-            options.id
+            options.packet.cache_id
         );
 
         if (!dest) {
             throw new Error(
-                `The destination with id=${options.id} could not be found in the ContextMenuStorage`
+                `The destination with id=${options.packet.cache_id} could not be found in the ContextMenuStorage`
             );
         }
 
--- source/lib/tiddlers/Inbox.ts
+++ source/lib/tiddlers/Inbox.ts
@@ -1,8 +1,12 @@
 import AbstractTiddler from "./AbstractTiddler";
 import recoder from "../formatting/recoder";
 import { EConfigKey } from "../../enums";
+import { IDispatchOptions } from "../../types";
 class Inbox extends AbstractTiddler {
-    async populateTitle() {
+    async populateTitle(options: IDispatchOptions) {
+        if (!options) {
+            throw new Error("options is a required parameter");
+        }
         const title = await this._configStorage.get(
             EConfigKey.TIDDLER_INBOX_TITLE
         );
