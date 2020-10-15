62adca0c4ef11af1f3c90e930ed81a14828bdfce
Destin Moulton <destin.moulton@gmail.com>
2020-10-15 14:25:35 -0600

    Added timeout on notifications to clear them

--- source/lib/notify.ts
+++ source/lib/notify.ts
@@ -1,11 +1,16 @@
 import { browser } from "webextension-polyfill-ts";
 async function notify(content: string) {
-    return browser.notifications.create({
+    const id = await browser.notifications.create({
         iconUrl: "/icon.png",
         title: "TiddlyBench",
         message: content,
         type: "basic",
     });
+
+    // Remove the notification after a bit
+    setTimeout(async () => {
+        await browser.notifications.clear(id);
+    }, 5000);
 }
 
 export default notify;
