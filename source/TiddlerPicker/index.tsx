d168af68c2686a9509cfdf914dde15dfb1cec6c1
Destin Moulton <destin.moulton@gmail.com>
2020-12-07 18:34:07 -0700

    Renamed to TiddlerPicker

---
+++ source/TiddlerPicker/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TiddlerPicker from "./TiddlerPicker";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <TiddlerPicker />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
