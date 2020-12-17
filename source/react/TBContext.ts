97c829698a049fe4ab7ebec3eb29e61f411743a7
Destin Moulton <destin.moulton@gmail.com>
2020-12-17 12:43:39 -0700

    Added tiddly bench objects

--- source/react/TBContext.ts
+++ source/react/TBContext.ts
@@ -1,11 +1,20 @@
 import React from "react";
 
+import ConfigStorage from "../lib/storage/ConfigStorage";
+import ContextMenuStorage from "../lib/storage/ContextMenuStorage";
 import Messenger from "../lib/Messenger";
+import TabsManager from "../lib/TabsManager";
 class TBServices {
     public messenger: Messenger;
+    public configStorage: ConfigStorage;
+    public contextMenuStorage: ContextMenuStorage;
+    public tabsManager: TabsManager;
 
     constructor() {
+        this.configStorage = new ConfigStorage();
+        this.contextMenuStorage = new ContextMenuStorage(this.configStorage);
         this.messenger = new Messenger();
+        this.tabsManager = new TabsManager();
     }
 }
 
