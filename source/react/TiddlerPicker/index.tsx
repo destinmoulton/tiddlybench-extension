18951306a9af879d50e1c8454826edfeeb80f127
Destin Moulton <destin.moulton@gmail.com>
2020-12-17 12:44:43 -0700

    Added TBContext as a provider

--- source/react/TiddlerPicker/index.tsx
+++ source/react/TiddlerPicker/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import TiddlerPicker from "./TiddlerPicker";
 
+import { TBContext, tbServices } from "../TBContext";
+
 ReactDOM.render(
     <React.StrictMode>
-        <TiddlerPicker />
+        <TBContext.Provider value={tbServices}>
+            <TiddlerPicker />
+        </TBContext.Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
